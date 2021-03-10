657e2ac265459ca40431f3e0e66c69e71df66fa9
Alexander <monzonmontalvo@gmail.com>
2021-03-09 23:20:27 -0500

    Actualizacion 3

---
+++ configs/dist/mongo-config.dev.js
@@ -0,0 +1,7 @@
+"use strict";
+
+//const direccion="mongodb://localhost:27017/[DataEComerce][DataCollection]";
+//const direccion="mongodb+srv://admin:<admin>@cluster0.ikple.mongodb.net/test"
+var direccion = "mongodb+srv://unsaac:unsaac123@primero.6gh6z.mongodb.net/EcommerceUnsaacBD?retryWrites=true&w=majority"; //const direccion="mongodb+srv://unsaac:unsaac123@primero.6gh6z.mongodb.net/EcommerceUnsaac?retryWrites=true&w=majority"
+
+module.exports = direccion;
