a0cde89dd7b363973a80cef3717b1ae0de2c8427
alexojo <75278388+alexojo@users.noreply.github.com>
2021-03-11 09:13:02 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -46,14 +46,11 @@ router.post('/ingresoproducts', function (req, res, next) {
 
   for (let i = 0; i < array_tags.length; i++){
     array_tags[i]=array_tags[i].toLocaleLowerCase();
-    if( i === (array_tags.length-1) ){
-      if (array_tags.indexOf(color.toLocaleLowerCase())===-1){
-        array_tags.push(color.toLocaleLowerCase());
-      }
-    }
+  }
+  if (array_tags.indexOf(color.toLocaleLowerCase())===-1){
+    array_tags.push(color.toLocaleLowerCase());
   }
   
-  array_tags.push(color.toLocaleLowerCase());
   var newProduct = new Product({
       imagePath: imagePath,
       title: title.toLocaleLowerCase(),
